9fe90e4846b8c315defb184c85015b81a0631dd7
TichaTin <ticha.tin@gmail.com>
2022-12-22 17:18:38 +1100

    init setup

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+/** @type {HTMLCanvasElement} */ // to suguest all Canvas built-in props and methods
+
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+CANVAS_WIDTH = canvas.width = 500;
+CANVAS_HEIGHT = canvas.height = 1000;
